435c99e6bba7c84e08a24b596d32094f8ff118e9
Sana Sheeraz <sana278.a@gmail.com>
2023-02-21 22:51:52 +0500

    'ReactJSTemplateAdded'

---
+++ hunger-block/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/home1';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
